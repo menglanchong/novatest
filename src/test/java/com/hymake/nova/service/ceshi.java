101868c194209fe9902c79aec689866cc1cc41d8
menglanchong <lc273911>
2018-01-16 09:11:35 +0800

    提交测试代码

--- src/test/java/com/hymake/nova/service/ceshi.java
+++ src/test/java/com/hymake/nova/service/ceshi.java
@@ -1,6 +1,12 @@
 package com.hymake.nova.service;
 
+import com.hymake.nova.page.DangerSourcePage;
+import com.hymake.nova.page.SafetyPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/1/15.
@@ -16,5 +22,15 @@ public class ceshi extends CommonMethod{
         total=total.replaceAll("共","").replaceAll("条","").trim();//获取中间条数， 文字替换成空格并去除空格+
         int t=Integer.parseInt(total);
         return t;
+    }
+    public static void  isData(){
+        int t =DangerSourceService.count(SafetyPage.DATA);
+        if(t!=0){
+           List<WebElement> first= driver.findElements(By.className(""));
+           first.get(0).click();
+        }else {
+
+
+        }
     }
 }
